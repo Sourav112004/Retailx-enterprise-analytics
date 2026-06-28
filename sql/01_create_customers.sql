f4871bd97ad28afd37aa516813e27d7a96246abe
Sourav Prakash <158016775+Sourav112004@users.noreply.github.com>
2026-06-28 15:59:15 +0530

    Update 01_create_customers.sql

---
+++ sql/01_create_customers.sql
@@ -0,0 +1,10 @@
+create database retailx;
+use retailx;
+
+CREATE TABLE customers (
+    customer_id CHAR(32) PRIMARY KEY,
+    customer_unique_id CHAR(32) NOT NULL,
+    customer_zip_code_prefix INT NOT NULL,
+    customer_city VARCHAR(100) NOT NULL,
+    customer_state CHAR(2) NOT NULL
+);
