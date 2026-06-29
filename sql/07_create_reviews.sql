dd97f2305715362f3a0eb66b001444ccd2655842
Sourav Prakash <158016775+Sourav112004@users.noreply.github.com>
2026-06-29 11:47:28 +0530

    Update 07_create_reviews.sql

---
+++ sql/07_create_reviews.sql
@@ -0,0 +1,16 @@
+CREATE TABLE reviews(
+review_id CHAR(32) NOT NULL,
+order_id CHAR(32) NOT NULL,
+review_score INT,
+review_comment_title TEXT,
+review_comment_message 	VARCHAR(50),
+review_creation_date DATETIME,
+review_answer_timestamp DATETIME,
+
+CONSTRAINT fk_reviews
+FOREIGN KEY(order_id)
+REFERENCES orders(order_id),
+
+PRIMARY KEY(review_id,order_id)
+
+)
