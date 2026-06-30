cb5bdb703a9770a80001f99addac6cce7c66f6af
Sourav Prakash <158016775+Sourav112004@users.noreply.github.com>
2026-06-30 13:51:45 +0530

    Update 08_import_data.sql

--- sql/08_import_data.sql
+++ sql/08_import_data.sql
@@ -1,1 +1,67 @@
 
+USE retailx;
+
+LOAD DATA LOCAL INFILE 'C:/MySQLUploads/olist_customers_dataset.csv'
+INTO TABLE customers
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'C:/MySQLUploads/olist_products_dataset.csv'
+INTO TABLE products
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(
+product_id,
+product_category_name,
+@product_name_length,
+@product_description_length,
+@product_photos_qty,
+@product_weight_g,
+@product_length_cm,
+@product_height_cm,
+@product_width_cm
+)
+SET
+product_name_length = NULLIF(@product_name_length,''),
+product_description_length = NULLIF(@product_description_length,''),
+product_photos_qty = NULLIF(@product_photos_qty,''),
+product_weight_g = NULLIF(@product_weight_g,''),
+product_length_cm = NULLIF(@product_length_cm,''),
+product_height_cm = NULLIF(@product_height_cm,''),
+product_width_cm = NULLIF(@product_width_cm,'');
+
+
+LOAD DATA LOCAL INFILE 'C:/MySQLUploads/olist_sellers_dataset.csv'
+INTO TABLE sellers
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+LOAD DATA LOCAL INFILE 'C:/MySQLUploads/olist_orders_dataset.csv'
+INTO TABLE orders
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(
+order_id,
+customer_id,
+order_status,
+@order_purchase_timestamp,
+@order_approved_at,
+@order_delivered_carrier_date,
+@order_delivered_customer_date,
+@order_estimated_delivery_date
+)
+SET
+order_purchase_timestamp = NULLIF(@order_purchase_timestamp,''),
+order_approved_at = NULLIF(@order_approved_at,''),
+order_delivered_carrier_date = NULLIF(@order_delivered_carrier_date,''),
+order_delivered_customer_date = NULLIF(@order_delivered_customer_date,''),
+order_estimated_delivery_date = NULLIF(@order_estimated_delivery_date,'');
