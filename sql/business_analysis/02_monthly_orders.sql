bfae2e05fcf9ad96927358e8fe2ce641e3cec040
Sourav Prakash <158016775+Sourav112004@users.noreply.github.com>
2026-07-03 13:02:26 +0530

    Update 02_monthly_orders.sql

--- sql/business_analysis/02_monthly_orders.sql
+++ sql/business_analysis/02_monthly_orders.sql
@@ -1,7 +1,1 @@
 
-with dec_cte AS(select DATE_FORMAT(order_purchase_timestamp, '%Y-%m') AS Monthly_sales,SUM(oi.price) AS revenue,COUNT(o.order_id) AS total_orders from orders o
-JOIN order_items oi on o.order_id=oi.order_id
-group by DATE_FORMAT(order_purchase_timestamp, '%Y-%m')
-order by revenue asc
-)
-select*from dec_cte;
