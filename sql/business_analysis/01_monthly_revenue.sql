50805435a36e96a874cd55390d1d8484a7cd368a
Sourav Prakash <158016775+Sourav112004@users.noreply.github.com>
2026-07-03 12:29:38 +0530

    Update 01_monthly_revenue.sql

--- sql/business_analysis/01_monthly_revenue.sql
+++ sql/business_analysis/01_monthly_revenue.sql
@@ -1,1 +1,6 @@
-
+select DATE_FORMAT(order_purchase_timestamp, '%Y-%m') AS Monthly_sales,SUM(oi.price) AS revenue from orders o
+JOIN order_items oi on o.order_id=oi.order_id
+group by DATE_FORMAT(order_purchase_timestamp, '%Y-%m')
+order by revenue asc
+LIMIT 5
+;
